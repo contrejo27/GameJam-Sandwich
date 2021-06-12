b52890931d25f72acf73d948b840b679e7ace0aa
Justin <jdr1813@yahoo.com>
2021-06-12 10:02:28 -0500

    added button functionality to the pause menu and restart menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,10 +8,15 @@ public class MainMenu : MonoBehaviour
     public void PlayGame()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1;
     }
     public void QuitGame()
     {
         Debug.Log("Quit");
         Application.Quit();
+    }
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
     }
 }
