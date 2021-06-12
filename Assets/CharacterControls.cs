1be07508c751b43c3e01f1ef08592acb41551b44
Justin <jdr1813@yahoo.com>
2021-06-11 19:10:54 -0500

    Added a ground check for jumping to only jump once.

--- Assets/CharacterControls.cs
+++ Assets/CharacterControls.cs
@@ -5,9 +5,9 @@ using UnityEngine;
 public class CharacterControls : MonoBehaviour
 {
     public float moveSpeed = 3;
-    private bool isGrounded = false;
+    private bool isGrounded = true;
     public float jumpHeight = 100;
-
+    public int jumpsLeft = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +18,29 @@ public class CharacterControls : MonoBehaviour
     void Update()
     {
 
-        if (!isGrounded)
+        if (isGrounded && jumpsLeft > 0)
         {
             if (Input.GetButtonDown("Jump"))
             {
                 GetComponent<Rigidbody>().AddForce(Vector3.up * jumpHeight);
-
+                jumpsLeft--;
             }
         }
 
-        GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxis("Horizontal"), 0, 0) * moveSpeed);
+        GetComponent<Rigidbody>().AddForce(new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0) * moveSpeed);
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+            jumpsLeft++;
+        }
+        else
+        {
+            isGrounded = false;
+            print("not ground");
+        }
     }
 }
